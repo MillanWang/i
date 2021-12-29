3c21bb3f1a4e32dd26b9c2ba0804beac4a71fb0d
Millan <real.millan.wang@gmail.com>
2021-12-28 22:40:52 -0500

    setup navigation bar with basic routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
+import * as React from 'react';
+import HeaderBar from './components/HeaderBar';
+
+const introductionNavSection = { title: "Introduction", url: "/" };
+const workExperienceNavSection = { title: "Work Experience", url: "/experience" };
+const projectsNavSection = { title: "Projects", url: "/projects" };
+const educationNavSection = { title: "Education", url: "/education" };
+const extracurricularsNavSection = { title: "Extracurriculars", url: "/extracurriculars" };
+
+const navigationSections =
+  [introductionNavSection,
+    workExperienceNavSection,
+    projectsNavSection,
+    educationNavSection,
+    extracurricularsNavSection
+  ];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="Header">
+        <HeaderBar sections={navigationSections} />
+      </div>
+
+      <div id="PageContent">
+        <Switch>
+          <Route exact path={introductionNavSection.url}>
+            <p>intro</p>
+          </Route>
+          <Route path={workExperienceNavSection.url}>
+            <p>work experience</p>
+          </Route>
+          <Route exact path={projectsNavSection.url}>
+            <p>projects</p>
+          </Route>
+          <Route exact path={educationNavSection.url}>
+            <p>education</p>
+          </Route>
+          <Route exact path={extracurricularsNavSection.url}>
+            <p>extracurrics</p>
+          </Route>
+        </Switch>
+      </div>{/* End PageContent */}
     </div>
   );
 }
---
+++ src/components/HeaderBar.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Link } from "react-router-dom";
+
+const pageTitle = 'Millan Wang';
+
+function HeaderBar(props) {
+    return (
+        <React.Fragment>
+            {/* Top title toolbar */}
+            <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Typography
+                    component="h2"
+                    variant="h5"
+                    color="inherit"
+                    align="center"
+                    noWrap
+                    sx={{ flex: 1 }}
+                >
+                    {pageTitle}
+                </Typography>
+            </Toolbar>
+            {/* Top navigation toolbar */}
+            <Toolbar
+                component="nav"
+                variant="dense"
+                sx={{ justifyContent: 'space-between' }}
+            >
+                {props.sections.map((section) => (
+                    <Link to={section.url}>
+                        <Button>
+                            {section.title}
+                        </Button>
+                    </Link>
+                ))}
+            </Toolbar>
+        </React.Fragment>
+    );
+}
+
+export default HeaderBar;
