b9658284eb320120397ba64fc3022efe3ecc0c12
Millan Wang <real.millan.wang@gmail.com>
2025-12-04 02:03:36 -0500

    minor content updates

--- src/components/navigationPages/IntroPage.tsx
+++ src/components/navigationPages/IntroPage.tsx
@@ -1,41 +1,39 @@
-import * as React from 'react';
-import { ImageLinkCardProps } from '../cards/ImageLinkCard';
-import TwoColumnCardGrid from '../cards/TwoColumnCardGrid';
+import * as React from "react";
+import { ImageLinkCardProps } from "../cards/ImageLinkCard";
+import TwoColumnCardGrid from "../cards/TwoColumnCardGrid";
 
 // Data
-import IntroPageData from '../../data/introPageData.json';
+import IntroPageData from "../../data/introPageData.json";
 
 // Images
 import MillPortrait from "../../images/MillPortrait.jpg";
-import MillTunnel from '../../images/Mill_Tunnel.jpg';
+import MillTunnel from "../../images/Mill_Tunnel.jpg";
 
 const leftColumnCards: ImageLinkCardProps[] = [
-    IntroPageData.HelloCard,
-    {
-        ...IntroPageData.MillWithMap,
-        img: MillPortrait
-    },
+  IntroPageData.HelloCard,
+  {
+    ...IntroPageData.MillWithMap,
+    img: MillPortrait,
+  },
 ];
 
 const rightColumnCards: ImageLinkCardProps[] = [
-    IntroPageData.AboutMe,
-    IntroPageData.LinksCard,
-    IntroPageData.Technologies,
-    {
-        ...IntroPageData.MillTunnel,
-        img: MillTunnel,
-    }
+  IntroPageData.Technologies,
+  {
+    ...IntroPageData.MillTunnel,
+    img: MillTunnel,
+  },
 ];
 
 function IntroPage() {
-    return (
-        <TwoColumnCardGrid
-            leftColumnStackHeader=""
-            leftColumnCardProps={leftColumnCards}
-            rightColumnStackHeader=""
-            rightColumnCardProps={rightColumnCards}
-        />
-    )
-};
+  return (
+    <TwoColumnCardGrid
+      leftColumnStackHeader=""
+      leftColumnCardProps={leftColumnCards}
+      rightColumnStackHeader=""
+      rightColumnCardProps={rightColumnCards}
+    />
+  );
+}
 
 export default IntroPage;
